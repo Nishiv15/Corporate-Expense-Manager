bfda55f37928edbdc987817ec021d609d68ed46c
Nishiv15 <tnishiv@gmail.com>
2025-11-28 19:55:51 +0530

    index.js setup

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+// database
+connectDB();
+
+// test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
